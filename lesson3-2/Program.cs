d27848d78dd3e0111dfe4ee01e8ba7f5b50b3a4b
Lydia <frankenshteynle@gmail.com>
2023-11-29 15:36:10 +0300

    Third lesson, task 2

---
+++ lesson3-2/Program.cs
@@ -0,0 +1,40 @@
+﻿class Program
+{
+    public static void Main(string [] arg)
+    {   
+        int [] array = CreateArray(10,-100,100);
+        Console.WriteLine(Print(array));
+        int countNum = FindEvenElement(array);
+        Console.WriteLine($"Количество четных чисел в массиве = {countNum}");
+    }
+
+    public static int [] CreateArray(int size, int min, int max)
+    {
+        int [] array = new int [size];
+        Random random = new();
+        for (int i = 0; i < size; i++)
+        {
+            array[i] = random.Next(min, max + 1);
+        }
+        return array;
+    }
+
+    public static string Print(int [] array)
+    {
+        string result = string.Join(", ", array);
+        return result;
+    }
+
+    public static int FindEvenElement(int [] array)
+    {
+        int count = 0;
+        foreach(int item in array)
+        {
+            if (item % 2 == 0)
+            {
+                count++;
+            }
+        }
+        return(count);
+    }
+}
