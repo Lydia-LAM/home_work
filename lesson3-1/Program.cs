c7d6f0ee8f49c670f04e6a404e5d474f5d1c2b44
Lydia <frankenshteynle@gmail.com>
2023-11-29 15:31:03 +0300

    Third lesson, task 1

---
+++ lesson3-1/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+
+class Program
+{
+    public static void Main(string [] arg)
+    {   
+        int [] array = CreateArray(10,1,100);
+        Console.WriteLine(Print(array));
+        int countNum = FindElement(array,20,90);
+        Console.WriteLine($"Количество элементов массива, значения которых лежат искомом отрезке = {countNum}");
+    }
+
+    public static int [] CreateArray(int size, int min, int max)
+    {
+        int [] array = new int [size];
+        Random random = new();
+        for (int i = 0; i < size; i++)
+        {
+            array[i] = random.Next(min, max + 1);
+        }
+        return array;
+    }
+
+    public static string Print(int [] array)
+    {
+        string result = string.Join(", ", array);
+        return result;
+    }
+
+    public static int FindElement(int [] array, int minRange, int maxRange)
+    {
+        int count = 0;
+        foreach(int item in array)
+        {
+            if (item <= maxRange && item >= minRange)
+            {
+                count++;
+            }
+        }
+        return(count);
+    }
+}
